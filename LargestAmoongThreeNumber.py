9aa3ab4278c23080034744f26a3c5b2581a7b7b8
KamalAres <53576033+KamalAres@users.noreply.github.com>
2019-08-07 06:22:59 +0530

    Add files via upload

---
+++ LargestAmoongThreeNumber.py
@@ -0,0 +1,10 @@
+#Program to find the largest among three numbers
+a=int(input("Enter the first Number: "))
+b=int(input("Enter the second Number: "))
+c=int(input("Enter the thrid Number: "))
+if a>b and a>c:
+    print(a," is the largest Number.")
+elif b>c:
+    print(b," is the largest Number.")
+else:
+    print(c," is the largest Number.")
---
+++ OddOrEven.py
@@ -0,0 +1,6 @@
+#Program to check the number is odd or even
+n=int(input("Enter the Number:"))
+if n%2==0:
+    print("The number is Even.")
+else:
+    print("The number is Odd.")
